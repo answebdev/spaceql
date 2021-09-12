9278a81fef01a709f120108a49dd0aa012399f7d
ANS <36783010+answebdev@users.noreply.github.com>
2021-09-11 18:57:56 -0700

    add logo

--- server.js
+++ server.js
@@ -4,12 +4,6 @@ const schema = require('./schema');
 
 const app = express();
 
-// Video 1: https://www.youtube.com/watch?v=SEMTj8w04Z8
-// Video 2: https://www.youtube.com/watch?v=-XwkFm5a9lw
-// Code: https://github.com/bradtraversy/spacex_launch_stats
-// SpaceX API: https://github.com/r-spacex/SpaceX-API
-// SpaceX API Docs: https://docs.spacexdata.com/
-
 app.use(
   '/graphql',
   graphqlHTTP({
