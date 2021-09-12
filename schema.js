f298dd377c0485b493395476b78f428b5122b94f
ANS <36783010+answebdev@users.noreply.github.com>
2021-09-11 18:09:40 -0700

    add root query

--- schema.js
+++ schema.js
@@ -1,9 +1,12 @@
 // All the GraphQL stuff goes here in this file.
+const axios = require('axios');
 const {
   GraphQLObjectType,
   GraphQLInt,
   GraphQLString,
   GraphQLBoolean,
+  GraphQLList,
+  GraphQLSchema,
 } = require('graphql');
 
 // OBJECT TYPES:
@@ -29,4 +32,23 @@ const RocketType = new GraphQLObjectType({
     rocket_name: { type: GraphQLString },
     rocket_type: { type: GraphQLString },
   }),
+});
+
+// Root Query
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    launches: {
+      type: new GraphQLList(LaunchType),
+      resolve(parent, args) {
+        return axios
+          .get('https://api.spacexdata.com/v3/launches')
+          .then((res) => res.data);
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
 });
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const graphqlHTTP = require('express-graphql');
-const schema = require('./schema')
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema');
 
 const app = express();
 
@@ -16,4 +16,4 @@ app.use(
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}...`));
